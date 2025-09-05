adeaf5f7085f81d827c3b2d025aa3d2e7c8442ec
Lesley Terol <testerles76@gmail.com>
2025-09-05 14:22:51 +0200

    Add initial implementation of API request with validation

---
+++ get-api.js
@@ -0,0 +1,22 @@
+import http from 'k6/http'
+import { check } from 'k6'
+
+export const options = {
+    vus: 10,
+    iterations: 20
+}
+const params = {
+    headers: {
+        // 'Authorization': 'Bearer 737433476'
+    }
+}
+
+const url ="https://gorest.co.in/public/v2/users/";
+
+export default function () {
+
+    const response = http.get(url, params)
+    check(response, {
+        'status code validation': (response) => response.status === 200
+    })
+}
